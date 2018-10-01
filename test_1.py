2216206dbf4b6529dc5b7d766cc5bde84ad7ac78
user <lirz2@inbox.lv>
2018-10-01 15:28:37 +0300

    20181001_15_28

---
+++ test_1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+a = 10
+b = 3.89
+c = 'P'
+# https://www.cplusplus.com/reference/cstdio/printf//
+print("a = %d"%(a))
+print("b = %.4f"%(b))
+print("c = %s"%(c))
+
+a = 56
+b = 8.96
+c = 'F'
+
+print("a = %d"%(a))
+print("b = %.4f"%(b))
+print("c = %s"%(c))
+
