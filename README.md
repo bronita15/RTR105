b7923408e571bbabc4b7337e6dfabbd28f3095e2
user <lirz2@inbox.lv>
2018-11-05 15:22:36 +0200

    20181105_15_22

--- README.md
+++ README.md
@@ -24,3 +24,4 @@ x >= y               # x is greater than or equal to y
 x <= y               # x is less than or equal to y
 x is y               # x is the same as y
 x is not y           # x is not the same as y
+
---
+++ dgr_20181105.py
@@ -0,0 +1,56 @@
+Python 2.7.12 (default, Dec  4 2017, 14:50:18) 
+[GCC 5.4.0 20160609] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> fruit = 'banana'
+>>> letter = fruit[1]
+>>> print(letter)
+a
+>>> x = 3
+>>> w = fruit[x-1]
+>>> print(w)
+n
+>>> fruit = 'banana'
+>>> print(len(fruit))
+6
+>>> word = 'banana'
+>>> count = 0
+>>> for letter == 'a':
+	
+SyntaxError: invalid syntax
+>>> for letter in word :
+	if letter == 'a' :
+		count = count + 1
+	print(count)
+
+	
+0
+1
+1
+2
+2
+3
+>>> s = 'Monty Python'
+>>> print(s[0:4])
+Mont
+>>> print(s[6:7])
+P
+>>> print(s[6:20])
+Python
+>>> print(s[:2])
+Mo
+>>> print(s[8:])
+thon
+>>> print(s[:])
+Monty Python
+>>> 
+>>> a = 'Hello'
+>>> b = a + 'There'
+>>> print(b)
+HelloThere
+>>> c = a + '' + 'There'
+>>> print(c)
+HelloThere
+>>> c = a + ' ' + 'There'
+>>> print(c)
+Hello There
+>>> 
