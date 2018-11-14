1bddc5a97d04eb133a7380956a2e7f65bab01ad0
Zanis <lirz2@inbox.lv>
2018-11-14 22:30:24 +0200

    20181114_22_30

---
+++ exp.py
@@ -0,0 +1,16 @@
+from decimal import Decimal
+import math
+
+def my_exp(x):
+    k = 0
+    a = (1-x)*((-1)**k*x**k/(1))
+    S = a
+    while k < 1000:
+        k = k + 1
+        a = a * (1-x)*((-1)**k*x**k/ (math.factorial(k)))
+        S = S + a
+        print(k,a,S)
+
+x = float(input("Lietotaj, ludzu ievadi argumentu (x): "))
+y = my_exp(x)
+print(x,y)
