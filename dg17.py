4052cfb0a5adfca81bf2f442ea9de0879e7170e2
user <lirz2@inbox.lv>
2018-11-26 15:46:47 +0200

    20181126_15_46

---
+++ dg17.py
@@ -0,0 +1,8 @@
+Python 3.6.5 (default, Apr  1 2018, 05:46:30) 
+[GCC 7.3.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+===================== RESTART: /home/user/RTR105/170.py =====================
+Dotajaa intervaalaa sakne(s) ir!
+Saknes ir: 3.1425781250000004
+>>> 
