a233ebfc45189efb8c7c9c80dfb65216f34dd2c1
user <lirz2@inbox.lv>
2018-11-29 11:42:18 +0200

    20181129_11_42

---
+++ history_20181129_11_41.py
@@ -0,0 +1,12 @@
+    1  VirtualBox --startvm XP
+    2  quartus
+    3  git clone https://github.com/bronita15/RTR105
+    4  ls -l
+    5  cd RTR105
+    6  idle
+    7  exit
+    8  ls -l
+    9  cd RTR105
+   10  idle
+   11  ls -l
+   12  history > history_20181129_11_41.py
